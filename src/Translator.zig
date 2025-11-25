90fa5cefbe764ca0577f791566f664799fb61827
Steeve Morin <steeve@zml.ai>
2025-11-25 18:21:15 +0100

    Fix incomplete bitCast in bit shifts
    Closes #217

--- src/Translator.zig
+++ src/Translator.zig
@@ -2705,7 +2705,7 @@ fn transShiftExpr(t: *Translator, scope: *Scope, bin: Node.Binary, op_id: ZigTag
     // lhs >> @intCast(rh)
     const lhs = try t.transExpr(scope, bin.lhs, .used);
 
-    const rhs = try t.transExprCoercing(scope, bin.rhs, .used);
+    const rhs = try t.transExpr(scope, bin.rhs, .used);
     const rhs_casted = try ZigTag.int_cast.create(t.arena, rhs);
 
     return t.createBinOpNode(op_id, lhs, rhs_casted);
